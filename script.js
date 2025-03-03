fc43fa8ff4030f99ddfd59c8a272b32979524cf9
ivey803 <spel0426@student.acitech.org>
2025-03-03 09:13:22 -0500

    added new text and center aligned every thing need to figure out how to add a couple more things

--- script.js
+++ script.js
@@ -1,7 +1,56 @@
-const button1 = document. getElementById("button1");
+let text = document.getElementById("text");
+let button1 = document.getElementById("button1");
+
+
+
+let clicks=0;
 
 button1.addEventListener("click", yell_at_user);
 
 function yell_at_user() {
-    alert("Hey it says do not press")
+   clicks++;
+   if( clicks == 1 ){
+text.innerHTML = "hey Stop";
+   }
+   if( clicks == 3 ){
+    text.innerHTML = "hey man read the button";
+       }
+       if( clicks == 5 ){
+        text.innerHTML = "... stop";
+           }
+           if( clicks == 7 ){
+            text.innerHTML = "You may regret it";
+               }
+               if( clicks == 9 ){
+                text.innerHTML = "i guess you won't stop pressing the button";
+                   }
+                   if( clicks == 11 ){
+                     text.innerHTML = "Wait...";
+                        } 
+                        if( clicks == 13 ){
+                           text.innerHTML = "You know what can't stop";
+                              }
+                              if( clicks == 15 ){
+                                 text.innerHTML = "A goat";
+                                    }
+                                    if( clicks == 17 ){
+                                       text.innerHTML = "you know who else is the goat";
+                                          }
+                                          if( clicks == 19 ){
+                                             text.innerHTML = "LEBRON";
+                                                }
+                                                if( clicks == 20 ){
+                                                   text.innerHTML = "BOOM BOOM BOOM BOOM";
+                                                      }
+                                                      if( clicks == 21 ){
+                                                         text.innerHTML = "But serously stop pressing the button";
+                                                            }
+                                                            if( clicks == 22 ){
+                                                               text.innerHTML = "something bad might happent to Lebron";
+                                                                  }
+                                                                  if( clicks == 23 ){
+                                                                     text.innerHTML = "if you press it again hes going to retire";
+                                                                        }
+
+
 }
